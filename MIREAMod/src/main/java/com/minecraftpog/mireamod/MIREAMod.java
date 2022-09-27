660c7c28e054c78ca8080bb9595483c77d5b41e1
Anastasia Karpova <vip.anas2003@inbox.ru>
2022-09-27 11:47:43 +0300

    Initial commit

---
+++ MIREAMod/src/main/java/com/minecraftpog/mireamod/MIREAMod.java
@@ -0,0 +1,16 @@
+package com.minecraftpog.mireamod;
+
+import com.minecraftpog.mireamod.core.init.ItemInit;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+
+@Mod(MIREAMod.MODID)
+public class MIREAMod {
+	public static final String MODID = "mireamod";
+	
+	public MIREAMod() {
+		var bus = FMLJavaModLoadingContext.get().getModEventBus();
+		
+		ItemInit.ITEMS.register(bus);
+	}
+}
