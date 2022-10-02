6f4be55547453fca1a1ffc115581243cdf78d9f7
Anastasia Karpova <vip.anas2003@inbox.ru>
2022-10-02 15:06:02 +0300

    Added initial custom dimension

--- MIREAMod/src/main/java/com/minecraftpog/mireamod/MIREAMod.java
+++ MIREAMod/src/main/java/com/minecraftpog/mireamod/MIREAMod.java
@@ -1,6 +1,8 @@
 package com.minecraftpog.mireamod;
 
 import com.minecraftpog.mireamod.core.init.ItemInit;
+import com.minecraftpog.mireamod.world.dimension.ModDimensions;
+
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
@@ -12,5 +14,6 @@ public class MIREAMod {
 		var bus = FMLJavaModLoadingContext.get().getModEventBus();
 		
 		ItemInit.ITEMS.register(bus);
+		ModDimensions.register();
 	}
 }
