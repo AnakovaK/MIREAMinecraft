cdd85fa089eed41ca836c3cb32334aa989f699a9
Anastasia Karpova <vip.anas2003@inbox.ru>
2022-10-03 17:26:50 +0300

    Added initial structures spawn

--- MIREAMod/src/main/java/com/minecraftpog/mireamod/MIREAMod.java
+++ MIREAMod/src/main/java/com/minecraftpog/mireamod/MIREAMod.java
@@ -2,6 +2,7 @@ package com.minecraftpog.mireamod;
 
 import com.minecraftpog.mireamod.core.init.ItemInit;
 import com.minecraftpog.mireamod.world.dimension.ModDimensions;
+import com.minecraftpog.mireamod.world.structure.ModStructures;
 
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -9,11 +10,14 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(MIREAMod.MODID)
 public class MIREAMod {
 	public static final String MODID = "mireamod";
+
 	
 	public MIREAMod() {
 		var bus = FMLJavaModLoadingContext.get().getModEventBus();
 		
 		ItemInit.ITEMS.register(bus);
+		ModStructures.register(bus);
+		
 		ModDimensions.register();
 	}
 }
