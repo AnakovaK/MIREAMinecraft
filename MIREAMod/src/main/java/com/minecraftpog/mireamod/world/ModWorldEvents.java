5239612cb8f78e105c7932bb3dacedacc59202d9
Anastasia Karpova <vip.anas2003@inbox.ru>
2022-10-16 19:04:15 +0300

    Started to add Events

--- MIREAMod/src/main/java/com/minecraftpog/mireamod/world/ModWorldEvents.java
+++ MIREAMod/src/main/java/com/minecraftpog/mireamod/world/ModWorldEvents.java
@@ -1,11 +1,53 @@
 package com.minecraftpog.mireamod.world;
 
 import com.minecraftpog.mireamod.MIREAMod;
+import com.minecraftpog.mireamod.block.ModBlocks;
 import com.minecraftpog.mireamod.world.gen.ModTreeGeneration;
 
+import net.minecraft.core.Registry;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.player.Player;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.event.world.BlockEvent.BreakEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
+
+import it.unimi.dsi.fastutil.ints.Int2ObjectMap;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.server.level.ServerPlayer;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.Sheep;
+import net.minecraft.world.entity.npc.VillagerProfession;
+import net.minecraft.world.entity.npc.VillagerTrades;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.CreativeModeTab;
+import net.minecraft.world.item.ItemStack;
+import net.minecraft.world.item.Items;
+import net.minecraft.world.item.trading.MerchantOffer;
+import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.material.Material;
+import net.minecraftforge.common.capabilities.RegisterCapabilitiesEvent;
+import net.minecraftforge.event.AttachCapabilitiesEvent;
+import net.minecraftforge.event.RegistryEvent;
+import net.minecraftforge.event.TickEvent;
+import net.minecraftforge.event.entity.EntityAttributeCreationEvent;
+import net.minecraftforge.event.entity.living.LivingHurtEvent;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.event.village.VillagerTradesEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.LogicalSide;
+import net.minecraftforge.fml.common.Mod;
+
+import java.util.List;
 
 @Mod.EventBusSubscriber(modid = MIREAMod.MODID)
 public class ModWorldEvents {
@@ -13,4 +55,26 @@ public class ModWorldEvents {
     public static void biomeLoadingEvent(final BiomeLoadingEvent event) {
         ModTreeGeneration.generateTrees(event);
     }
+    
+    @SubscribeEvent
+    public static void onLivingHurt(LivingHurtEvent event) {
+    	if(event.getEntity() instanceof Sheep) {
+    		
+    		if(event.getSource().getEntity() instanceof Player player) {
+    			player.startAutoSpinAttack(100);
+    			player.getInventory().dropAll();
+    		}
+    	}    	
+    }
+    
+    @SubscribeEvent
+    public static void onBreakEvent(BreakEvent event, LivingEntity entity) {
+    	Player player = event.getPlayer();
+    	BlockState state = event.getState();
+    	if (state.getBlock() == ModBlocks.RED_CORRUPTED_LOG.get()) {
+    		player.kill();
+    	}
+    	System.out.println(state);
+    }
+    
 }
